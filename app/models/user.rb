1a6f21a779230b237352650e693a756dfe4a581b
onlystar1991 <onlystar1991@hotmail.com>
2015-07-31 23:30:16 +0200

    Mess

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-	attr_accessor :password, :first_name, :last_name, :phone_number, :email
+	#attr_accessor :password, :first_name, :last_name, :phone_number, :email
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
   validates :first_name, :presence => true, :length => { :in => 1..20 }
 	validates :last_name, :presence => true, :length => { :in => 1..20 }
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 	def setParams(params)
 		self.password = params[:password]
 		self.first_name = params[:first_name]
-		self.last_name = params[:name]
+		self.last_name = params[:last_name]
 		self.phone_number = params[:phone_number]
 		self.email = params[:email]
 	end
